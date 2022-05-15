5665950a473346e01a72fa8d1bbdcf969fabf0b2
Miguel Angel Torres Vargas <ingeniero.miguelvargas@gmail.com>
2022-05-14 22:23:42 -0500

    ADD Base

---
+++ source/app.py
@@ -0,0 +1,29 @@
+# import time
+# from selenium.webdriver.chrome.options import Options
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+#
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_prefs = {}
+# chrome_options.experimental_options["prefs"] = chrome_prefs
+# chrome_prefs["profile.default_content_settings"] = {"images": 2}
+#
+# driver = webdriver.Chrome(options=chrome_options)
+# driver.set_window_size(800, 600)
+#
+#
+# url = 'https://www.google.com/'
+# driver.get(url)
+# title = driver.title
+# print(title)
+
+import os
+
+from source.controller import Runner
+
+if __name__ == "__main__":
+    root_path = os.path.dirname(os.path.abspath(__file__))
+    Runner(root_path)
---
+++ source/helper/user_agent_browser.py
@@ -0,0 +1,48 @@
+from source.helper.complement import Complement
+
+
+class UserAgentBrowser:
+    path_file = None
+
+    def __init__(self, path_asset):
+        self.path_file = f"{path_asset}/user_agent.txt"
+        self.driver = None
+
+    def set_driver(self, driver):
+        self.driver = driver
+
+    def _get_user_agent(self):
+        command = "return navigator.userAgent"
+        data_user_agent = self.driver.execute_script(command)
+        Complement.write_file(self.path_file, data_user_agent)
+
+    def exist_user_agent(self):
+        return Complement.check_file_exist(self.path_file)
+
+    def data_user_agent(self):
+        if self.exist_user_agent():
+            return Complement.read_file(self.path_file)
+        else:
+            self._get_user_agent()
+            return self.data_user_agent()
+
+# # TEST
+# def main(web_driver=None):
+#     import os
+#     from helper.web_driver import WebDriver
+#
+#     # Get path asset
+#     path_asset = os.path.dirname(os.path.abspath(__file__))
+#     path_asset = path_asset.replace("helper", "assets")
+#     user_agent_browser = UserAgentBrowser(path_asset)
+#     if not user_agent_browser.exist_user_agent():
+#         if web_driver is None:
+#             chrome_driver = WebDriver(path_asset, "chrome")
+#             web_driver = chrome_driver.get_driver()
+#         user_agent_browser.set_driver(web_driver)
+#     user_agent = user_agent_browser.get_data_user_agent()
+#     return user_agent
+#
+#
+# if __name__ == "__main__":
+#     main()
