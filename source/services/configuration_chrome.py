087862ef5c656347a6a6f780cc0461b862e6b554
Miguel Angel Torres Vargas <ingeniero.miguelvargas@gmail.com>
2022-05-22 02:40:09 -0500

    FIX Process Config

--- source/services/configuration_chrome.py
+++ source/services/configuration_chrome.py
@@ -147,8 +147,8 @@ class ConfigurationChrome:
         return options_browser
 
     def _get_options_docker(self, options_browser):
-        options_browser.add_argument("--no-sandbox")
-        options_browser.add_argument("--disable-dev-shm-usage")
+        # options_browser.add_argument("--no-sandbox")
+        # options_browser.add_argument("--disable-dev-shm-usage")
         options_browser.add_argument("--headless")
         return options_browser
 
